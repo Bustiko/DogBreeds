a0507f23870076d65d2092adab255821fdac1324
Buse Karabıyık <karabiyikbuse04@gmail.com>
2025-01-28 20:54:49 +0300

    Initial Commit

---
+++ DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by Buse Karabıyık on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogBreedsTests/DogBreedsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsTests.swift
+//  DogBreedsTests
+//
+//  Created by Buse Karabıyık on 28.01.2025.
+//
+
+import Testing
+@testable import DogBreeds
+
+struct DogBreedsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
